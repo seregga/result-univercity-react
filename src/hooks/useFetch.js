f9dd41a7859a6d5d2c7a09f184bc831ba2499b45
Sergey <9249798@mail.ru>
2025-02-18 21:10:47 +0300

    feat: developed react hook useFetch
    Task: 1 in react_hooks

---
+++ src/hooks/useFetch.js
@@ -0,0 +1,37 @@
+import { useState, useEffect, useCallback, useRef } from 'react';
+
+export function useFetch(url) {
+    const [isLoading, setIsLoading] = useState(false)
+    const [data, setData] = useState([])
+    const [error, setError] = useState("")
+
+    const getData = useCallback(async (params = {}) => {
+        setIsLoading(true)
+        setError("")
+        try {
+            const r = await fetch(url)
+            const data = await r.json()
+
+            if (params.hasOwnProperty("_limit")) {
+                setData(data.slice(0, params._limit))
+            } else {
+                setData(data)
+            }
+
+        } catch (error) {
+            setError(error.message)
+        } finally {
+            setIsLoading(false)
+        }
+    }, [url])
+
+    useEffect(() => {
+        getData()
+    }, [getData])
+
+    const refetch = useCallback(({ params }) => {
+        getData(params)
+    }, [getData])
+
+    return { data, isLoading, error, refetch };
+}
