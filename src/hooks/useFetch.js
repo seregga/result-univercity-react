e75bdcf558e460486fa32d9e7f2807689da44c9b
Sergey <9249798@mail.ru>
2025-02-18 21:22:15 +0300

    fix: deleted import hook useRef

--- src/hooks/useFetch.js
+++ src/hooks/useFetch.js
@@ -1,4 +1,4 @@
-import { useState, useEffect, useCallback, useRef } from 'react';
+import { useState, useEffect, useCallback } from 'react';
 
 export function useFetch(url) {
     const [isLoading, setIsLoading] = useState(false)
