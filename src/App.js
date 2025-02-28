e4b43c63724834c3945a9bd47041dbe9fa179f6b
Sergey <9249798@mail.ru>
2025-02-28 15:32:59 +0300

    feat: developed react hook useHover
    Task: 3 in react_hooks

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import './App.css';
 // import {Demo} from './components/Demo'
-import {Demo2} from './components/Demo2'
+// import {Demo2} from './components/Demo2'
+import {Demo3} from './components/Demo3'
 
 function App() {
     return (
         <div className="App">
-            <Demo2 />
+            <Demo3 />
         </div>
     );
 }
